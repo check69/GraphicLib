9f3d19bcffa34131d76a02134e4718f8264fdd33
Antonio José Checa Bustos <antonio.checa.bustos@gmail.com>
2017-10-18 23:47:24 +0200

    Starting using widgets

--- graphic_library.py
+++ graphic_library.py
@@ -8,6 +8,10 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.pagelayout import PageLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.graphics import Canvas
+from kivy.graphics import Rectangle
 
 class MyWidget(Widget):
     pass
@@ -21,6 +25,13 @@ class MyApp(App):
     def build(self):
         #self.load_kv('MyApp.kv')
         self.title = "Graphic Test Framework"
+        widget = MyWidget()
+        widget.add_widget(Label(text="prueba"))
+        myCanvas = Canvas()
+        myCanvas.add(Rectangle(size=(350, 1), pos=(0, 500)))
+        widget.canvas = myCanvas
+        widget.add_widget(Button(text="Boton", pos=(100, 500)))
+        return widget
         return MyWidget()
         return PageLayout()
         return StackLayout()
